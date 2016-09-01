f16fb6e94f1e930b90e6b81818fbb910e65d6be4
Jeff <200006506@qq.com>
2016-09-01 18:43:50 +0800

    Add UserController

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,11 +1,3 @@
 defmodule Zizhixi.UserView do
   use Zizhixi.Web, :view
-
-  def render("detail.json", %{user: user}) do
-    %{
-      id: user.id,
-      username: user.username,
-      avatar: user.avatar,
-    }
-  end
 end
