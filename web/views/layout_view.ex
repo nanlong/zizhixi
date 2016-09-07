fda7b7a4b3ba78e8082297ee6cfd899a7afa9b84
Jeff <200006506@qq.com>
2016-09-07 14:18:47 +0800

    Update header navigation

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,21 @@
 defmodule Zizhixi.LayoutView do
   use Zizhixi.Web, :view
+
+  alias Zizhixi.{
+    GroupController,
+    GroupPostController,
+    GroupCommentController
+  }
+
+  def navigation(conn) do
+    group_controllers = [GroupController, GroupPostController, GroupCommentController]
+
+    [
+      {group_controllers, "小组", group_path(conn, :index)},
+      {[], "问答", "/"},
+      {[], "天工", "/"},
+      {[], "司南车", "/"},
+      {[], "五杂市集", "/"},
+    ]
+  end
 end
