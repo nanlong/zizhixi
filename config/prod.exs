4b38c1038faf77d0d693e1a876e841e97304947b
Jeff <200006506@qq.com>
2016-09-22 12:33:46 +0800

    Update host

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :zizhixi, Zizhixi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "zizhixi.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
