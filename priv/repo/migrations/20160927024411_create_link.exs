034791beff81c08625745d65660c99f27d74181b
Jeff <200006506@qq.com>
2016-09-27 10:45:32 +0800

    Add Link model

---
+++ priv/repo/migrations/20160927024411_create_link.exs
@@ -0,0 +1,17 @@
+defmodule Zizhixi.Repo.Migrations.CreateLink do
+  use Ecto.Migration
+
+  def change do
+    create table(:links, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :href, :text
+      add :description, :text
+      add :category_id, references(:link_categories, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+    create index(:links, [:category_id])
+
+  end
+end
---
+++ web/models/link.ex
@@ -0,0 +1,21 @@
+defmodule Zizhixi.Link do
+  use Zizhixi.Web, :model
+
+  schema "links" do
+    field :name, :string
+    field :href, :string
+    field :description, :string
+    belongs_to :category, Zizhixi.LinkCategory
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :href, :description, :category_id])
+    |> validate_required([:name, :href, :description, :category_id])
+  end
+end
