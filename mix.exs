c1e936a4c1628d4bb24d18b37751287859bd76d7
Jeff <200006506@qq.com>
2016-09-27 12:49:14 +0800

    Add new link

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Zizhixi.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_html, "~> 2.7.0"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
--- web/models/link_category.ex
+++ web/models/link_category.ex
@@ -22,4 +22,16 @@ defmodule Zizhixi.LinkCategory do
     from c in __MODULE__,
       where: is_nil(c.category_id)
   end
+
+  def generate(categories) do
+    categories
+    |> Enum.filter(fn category -> is_nil(category.category_id) end)
+    |> Enum.map(fn parent ->
+      childs = Enum.filter(categories, fn child ->
+        child.category_id == parent.id
+      end)
+      Map.put_new(%{}, parent.name, (for child <- childs, do: {child.name, child.id}))
+    end)
+    |> Enum.reduce(&Map.merge/2)
+  end
 end
