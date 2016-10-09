0a1e8ea5eac4e967661ad23c669841c9f1745ba5
Jeff <200006506@qq.com>
2016-10-09 11:34:17 +0800

    Improve question answer only one

--- web/views/ask_answer_view.ex
+++ web/views/ask_answer_view.ex
@@ -1,3 +1,34 @@
 defmodule Zizhixi.AskAnswerView do
   use Zizhixi.Web, :view
+
+  alias Zizhixi.{Repo, AskAnswer}
+  import Guardian.Plug, only: [authenticated?: 1, current_resource: 1]
+  import Ecto.Query
+
+  def answer?(conn, question) do
+    if authenticated?(conn) do
+      current_user = current_resource(conn)
+
+      answer_count = AskAnswer
+      |> where(question_id: ^question.id)
+      |> where(user_id: ^current_user.id)
+      |> Repo.all
+      |> Enum.count
+
+      answer_count > 0
+    else
+      false
+    end
+  end
+
+  def current_answer(conn, question) do
+    current_user = current_resource(conn)
+
+    AskAnswer
+    |> where(question_id: ^question.id)
+    |> where(user_id: ^current_user.id)
+    |> order_by([asc: :inserted_at])
+    |> first
+    |> Repo.one
+  end
 end
